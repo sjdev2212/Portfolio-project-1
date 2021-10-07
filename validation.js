e4a0696cf946e70f1d92a87eb0ca687de82b841d
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-10-07 20:36:45 +0500

    Add alett message to form

--- validation.js
+++ validation.js
@@ -1,14 +1,18 @@
 const email = document.getElementById('email');
-const validateBtn = document.getElementById('validate-btn');
+const validateBtn = document.getElementById('validation-btn');
+const alertMessage = document.querySelector('#alertMessage');
+
 
 function checkValidation(element, message) {
-  let letters = element.value;
-  for (let i = 0; i < letters.length; i += 1) {
-    if (element[i] === element[i].toUpperCase()) {
-      message.textContent = "Email should be in lowercase";
-      message.style.display = "block";
-    } else {
-      message.style.display = "none";
-    }
+   let letters = element.split("");
+   for (let i = 0; i < letters.length; i += 1) {
+     if (letters[i] === letters[i].toUpperCase()) 
+       {message.textContent = "Email should be in lowercase";
+       message.style.display = "block";
+     } else {
+       message.style.display = "none";
+     }
   }
-}
+ };
+
+// validateBtn.addEventListener('click', ()=> checkValidation(email, alertMessage));
